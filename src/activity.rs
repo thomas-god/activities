cb131a4a5586d075c1e884f7fdd3cc91db94925e
thomas-god <thomas.godlewski@icloud.com>
2025-08-22 10:19:51 +0200

    feat: scaffold activity parsing

---
+++ src/activity.rs
@@ -0,0 +1,61 @@
+use thiserror::Error;
+
+use crate::{
+    DataField, DataValue, FitParserError, Record, parse_records,
+    parser::types::global_messages::DeviceInfoField,
+};
+
+#[derive(Debug, Default)]
+pub struct Activity {
+    pub metadata: ActivityMetadata,
+}
+
+#[derive(Debug, Default)]
+pub struct ActivityMetadata {
+    pub product_name: Option<String>,
+}
+
+#[derive(Debug, Error)]
+pub enum ParseActivityError {
+    #[error("Unabale to parse .fit file")]
+    ParserError(#[from] FitParserError),
+}
+
+pub fn parse_activity(file: &str) -> Result<Activity, ParseActivityError> {
+    let records = parse_records(file)?;
+
+    for record in records.iter() {
+        if let Some(product_name) = find_product_name(record) {
+            return Ok(Activity {
+                metadata: ActivityMetadata {
+                    product_name: Some(product_name),
+                },
+            });
+        }
+    }
+
+    Ok(Activity::default())
+}
+
+fn find_product_name(record: &Record) -> Option<String> {
+    let Record::Data(data) = record else {
+        return None;
+    };
+
+    for field in data.fields.iter() {
+        if let DataField::DeviceInfo(DeviceInfoField::ProductName) = field.kind {
+            return field
+                .values
+                .iter()
+                .filter_map(|val| match val {
+                    DataValue::String(s) => Some(s.clone()),
+                    _ => None,
+                })
+                .collect::<Vec<String>>()
+                .first()
+                .cloned();
+        }
+    }
+
+    None
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+mod activity;
 mod parser;
 
+pub use crate::activity::{Activity, ParseActivityError, parse_activity};
 pub use crate::parser::types::{DataField, DataType, DataValue};
-pub use crate::parser::{ParseError, Record, parse_records};
+pub use crate::parser::{FitParserError, Record, parse_records};
