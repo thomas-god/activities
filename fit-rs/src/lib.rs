09f7f7018eb6596f42916bd6a38c5c2b9ad6e417
thomas-god <thomas.godlewski@icloud.com>
2025-08-29 10:23:03 +0200

    chore: remove `activity` module from lib

--- fit-rs/src/lib.rs
+++ fit-rs/src/lib.rs
@@ -1,7 +1,5 @@
-mod activity;
 mod parser;
 
-pub use crate::activity::{Activity, ParseActivityError};
 pub use crate::parser::types::DataValue;
-pub use crate::parser::types::generated::{FitEnum, MesgNum};
+pub use crate::parser::types::generated::*;
 pub use crate::parser::{DataMessage, DataMessageField, FitParserError, parse_fit_messages};
