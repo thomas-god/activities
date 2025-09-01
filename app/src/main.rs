7d97bcc12cf57548c7a38cb5463a27e8957f69e5
thomas-god <thomas.godlewski@icloud.com>
2025-09-01 17:31:59 +0200

    feat: scaffold basic file upload

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use axum::{
+    Router,
+    body::Bytes,
+    http::{
+        HeaderValue, Method, StatusCode,
+        header::{CONTENT_TYPE, COOKIE},
+    },
+    routing::{get, post},
+};
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
+
+#[tokio::main]
+async fn main() {
+    // start tracing
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .finish();
+    if let Err(err) = tracing::subscriber::set_global_default(subscriber) {
+        tracing::error!("Error while setting up tracing subscriber: {err:?}");
+    };
+
+    let origin = env::var("ALLOW_ORIGIN")
+        .unwrap_or("http://127.0.0.1:5173".to_string())
+        .parse::<HeaderValue>()
+        .expect("ALLOW_ORIGIN is not a valid origin");
+    tracing::info!("allowed origin: {origin:?}");
+
+    let app = Router::new()
+        .route(
+            "/",
+            get(|| async {
+                tracing::info!("Hello world");
+                "Hello, World!"
+            }),
+        )
+        .route("/activity", post(post_activity))
+        .layer(TraceLayer::new_for_http())
+        .layer(
+            CorsLayer::new()
+                .allow_headers([CONTENT_TYPE, COOKIE])
+                .allow_origin([origin])
+                .allow_methods([Method::GET, Method::POST]),
+        );
+
+    // run our app with hyper, listening globally on port 3001
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3001));
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    tracing::info!("App started");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn post_activity(bytes: Bytes) -> StatusCode {
+    tracing::info!("Hello from post_activity");
+    StatusCode::CREATED
 }
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { PUBLIC_APP_URL } from '$env/static/public';
+
+	let files: FileList | undefined = $state(undefined);
+	let file_upload_content: string = $state('');
+
+	const checkCanUpload = (fileList: FileList | undefined): fileList is FileList => {
+		if (fileList) {
+			return fileList.length > 0;
+		}
+		return false;
+	};
+
+	let can_upload = $derived.by(() => {
+		return checkCanUpload(files);
+	});
+
+	const postActivity = async (fileList: FileList | undefined) => {
+		if (!checkCanUpload(fileList)) {
+			return;
+		}
+
+		const file = fileList.item(0)!;
+
+		let res = await fetch(`${PUBLIC_APP_URL}/activity`, {
+			body: file,
+			method: 'POST'
+		});
+
+		console.log(res.status);
+	};
+</script>
+
+<h1>Welcome to activities</h1>
+<p>Upload a new activity</p>
+
+<input
+	type="file"
+	class="file-input"
+	accept=".fit"
+	bind:files
+	bind:value={file_upload_content}
+	id="activity_file"
+	name="activity file"
+/>
+<button class="btn" disabled={!can_upload} onclick={() => postActivity(files)}>Upload</button>
