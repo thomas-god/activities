208d51402aa9cc399abf1114f3c13d85ae11aff7
thomas-god <thomas.godlewski@icloud.com>
2025-09-02 20:19:11 +0200

    feat(lib): add basic cli wrapper around the lib

---
+++ fit-parser/src/bin/main.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+use fit_parser::parse_fit_file;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    run_cli()
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "fit-parser-cli")]
+#[command(about = "A CLI wrapper for parsing .fit files")]
+#[command(next_line_help = true)]
+struct Cli {
+    #[arg(long)]
+    file: String,
+}
+
+pub fn run_cli() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
+    let file = cli.file;
+
+    let messages = parse_fit_file(&file).unwrap();
+
+    for message in messages {
+        println!("{message:?}");
+    }
+
+    Ok(())
+}
