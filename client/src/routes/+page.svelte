8170c1f3c84c43c03a901ce73878cd5c6856d564
Matías Plaza <matias.plaza@elmy.fr>
2025-09-12 18:30:45 +0200

    style: modify style of activity list

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -31,14 +31,14 @@
 	});
 </script>
 
-<div class="mx-2 mb-2 sm:mx-auto sm:w-sm">
+<div class="sm:w-sm mx-2 mb-2 sm:mx-auto">
 	<ActivitiesUploader {activitiesUploadedCallback} />
 </div>
 
 {#if topMetric}
 	<div
 		bind:clientWidth={chartWidth}
-		class="mx-2 mt-5 rounded-box bg-base-100 shadow-md sm:mx-auto sm:w-2xl"
+		class="rounded-box bg-base-100 sm:w-2xl mx-2 mt-5 shadow-md sm:mx-auto"
 	>
 		<p class="text-center">{topMetric.title}</p>
 		<TrainingMetricsChart
@@ -50,6 +50,6 @@
 	</div>
 {/if}
 
-<div class="mx-2 mt-5 sm:mx-auto sm:w-lg">
+<div class="sm:w-md mx-2 mt-5 sm:mx-auto">
 	<ActivityList activityList={sorted_activities} />
 </div>
