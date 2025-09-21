a4d344dddeeb7fe7798926d11c4f7dc7e7e3cc17
thomas-god <thomas.godlewski@icloud.com>
2025-09-21 16:24:22 +0200

    feat(app): add placeholder bootstrap function for single and multi user

---
+++ app/src/bootstrap/multi_user.rs
@@ -0,0 +1,71 @@
+use std::{collections::HashMap, sync::Arc};
+
+use tokio::sync::Mutex;
+
+use crate::{
+    config::Config,
+    domain::services::{activity::ActivityService, training_metrics::TrainingMetricService},
+    inbound::{
+        http::{DisabledUserService, HttpServer},
+        parser::FitParser,
+    },
+    outbound::memory::{
+        InMemoryActivityRepository, InMemoryRawDataRepository, InMemoryTrainingMetricsRepository,
+    },
+};
+
+pub async fn bootsrap_multi_user() -> anyhow::Result<
+    HttpServer<
+        ActivityService<
+            InMemoryActivityRepository,
+            InMemoryRawDataRepository,
+            TrainingMetricService<InMemoryTrainingMetricsRepository, InMemoryActivityRepository>,
+        >,
+        FitParser,
+        TrainingMetricService<InMemoryTrainingMetricsRepository, InMemoryActivityRepository>,
+        DisabledUserService,
+    >,
+> {
+    // start tracing
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .finish();
+    if let Err(err) = tracing::subscriber::set_global_default(subscriber) {
+        tracing::error!("Error while setting up tracing subscriber: {err:?}");
+    };
+
+    let config = Config::from_env()?;
+
+    let activity_repository = Arc::new(Mutex::new(InMemoryActivityRepository::new(vec![])));
+    let raw_data_repository = InMemoryRawDataRepository::new(HashMap::new());
+    let training_metrics_repository = InMemoryTrainingMetricsRepository::new(HashMap::new());
+
+    let training_metrics_service = Arc::new(TrainingMetricService::new(
+        training_metrics_repository,
+        activity_repository.clone(),
+    ));
+    let activity_service = ActivityService::new(
+        activity_repository.clone(),
+        raw_data_repository,
+        training_metrics_service.clone(),
+    );
+    let user_service = DisabledUserService {};
+
+    let parser = FitParser {};
+
+    let http_server = HttpServer::new(
+        activity_service,
+        parser,
+        training_metrics_service,
+        user_service,
+        config,
+    )
+    .await?;
+
+    Ok(http_server)
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,12 @@
-use app::bootstrap::demo::bootsrap_demo;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let http_server = bootsrap_demo().await?;
+    let http_server = if cfg!(feature = "single-user") {
+        app::bootstrap::single_user::bootsrap_single_user().await?
+    } else if cfg!(feature = "multi-user") {
+        app::bootstrap::multi_user::bootsrap_multi_user().await?
+    } else {
+        app::bootstrap::demo::bootsrap_demo().await?
+    };
 
     http_server.run().await
 }
