9dd2ca31822c98d30336c8d1f44a23a09a40dd4a
thomas-god <thomas.godlewski@icloud.com>
2025-09-25 09:26:54 +0200

    feat: update multi-user deployment to use sqlite implementations

--- app/src/bootstrap/multi_user.rs
+++ app/src/bootstrap/multi_user.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, path::Path, sync::Arc};
+use std::{path::PathBuf, sync::Arc};
 
 use anyhow::Ok;
 use tokio::sync::Mutex;
@@ -13,20 +13,29 @@ use crate::{
         },
         parser::FitParser,
     },
-    outbound::memory::{
-        InMemoryActivityRepository, InMemoryRawDataRepository, InMemoryTrainingMetricsRepository,
+    outbound::{
+        fs::FilesystemRawDataRepository,
+        sqlite::{
+            activity::SqliteActivityRepository, training_metrics::SqliteTrainingMetricsRepository,
+        },
     },
 };
 
 pub async fn bootsrap_multi_user() -> anyhow::Result<
     HttpServer<
         ActivityService<
-            InMemoryActivityRepository,
-            InMemoryRawDataRepository,
-            TrainingMetricService<InMemoryTrainingMetricsRepository, InMemoryActivityRepository>,
+            SqliteActivityRepository<FilesystemRawDataRepository, FitParser>,
+            FilesystemRawDataRepository,
+            TrainingMetricService<
+                SqliteTrainingMetricsRepository,
+                SqliteActivityRepository<FilesystemRawDataRepository, FitParser>,
+            >,
         >,
         FitParser,
-        TrainingMetricService<InMemoryTrainingMetricsRepository, InMemoryActivityRepository>,
+        TrainingMetricService<
+            SqliteTrainingMetricsRepository,
+            SqliteActivityRepository<FilesystemRawDataRepository, FitParser>,
+        >,
         UserService<
             MagicLinkService<SqliteMagicLinkRepository, SMTPEmailProvider>,
             SqliteUserRepository,
@@ -49,23 +58,9 @@ pub async fn bootsrap_multi_user() -> anyhow::Result<
 
     let config = Config::from_env()?;
 
-    let activity_repository = Arc::new(Mutex::new(InMemoryActivityRepository::new(vec![])));
-    let raw_data_repository = InMemoryRawDataRepository::new(HashMap::new());
-    let training_metrics_repository = InMemoryTrainingMetricsRepository::new(HashMap::new());
-
-    let training_metrics_service = Arc::new(TrainingMetricService::new(
-        training_metrics_repository,
-        activity_repository.clone(),
-    ));
-    let activity_service = ActivityService::new(
-        activity_repository.clone(),
-        raw_data_repository,
-        training_metrics_service.clone(),
-    );
+    let (activity_service, parser, training_metrics_service) = build_activity_service().await?;
 
     let user_service = build_user_service().await?;
-
-    let parser = FitParser {};
 
     let http_server = HttpServer::new(
         activity_service,
@@ -90,6 +85,67 @@ fn build_mailer() -> anyhow::Result<SMTPEmailProvider> {
     Ok(mailer)
 }
 
+async fn build_activity_service() -> anyhow::Result<(
+    ActivityService<
+        SqliteActivityRepository<FilesystemRawDataRepository, FitParser>,
+        FilesystemRawDataRepository,
+        TrainingMetricService<
+            SqliteTrainingMetricsRepository,
+            SqliteActivityRepository<FilesystemRawDataRepository, FitParser>,
+        >,
+    >,
+    FitParser,
+    Arc<
+        TrainingMetricService<
+            SqliteTrainingMetricsRepository,
+            SqliteActivityRepository<FilesystemRawDataRepository, FitParser>,
+        >,
+    >,
+)> {
+    let root_path = PathBuf::from(load_env("ACTIVITIES_DATA_PATH")?);
+    let db_dir = root_path.clone().join("db/");
+    if !db_dir.exists() {
+        tokio::fs::create_dir_all(&db_dir).await?;
+    }
+    let raw_data_dir = root_path.clone().join("activities/");
+    if !raw_data_dir.exists() {
+        tokio::fs::create_dir_all(&raw_data_dir).await?;
+    }
+
+    let parser = FitParser {};
+
+    let raw_data_repository = FilesystemRawDataRepository::new(PathBuf::from(raw_data_dir));
+
+    let activity_db = db_dir.clone().join("activities.db");
+    let activity_repository = Arc::new(Mutex::new(
+        SqliteActivityRepository::new(
+            &format!("sqlite:{}", activity_db.to_string_lossy()),
+            raw_data_repository.clone(),
+            parser.clone(),
+        )
+        .await?,
+    ));
+
+    let trainin_metrics_db = db_dir.clone().join("training_metrics.db");
+    let training_metrics_repository = SqliteTrainingMetricsRepository::new(&format!(
+        "sqlite:{}",
+        trainin_metrics_db.to_string_lossy()
+    ))
+    .await?;
+
+    let training_metrics_service = Arc::new(TrainingMetricService::new(
+        training_metrics_repository,
+        activity_repository.clone(),
+    ));
+    let activity_service = ActivityService::new(
+        activity_repository.clone(),
+        raw_data_repository,
+        training_metrics_service.clone(),
+    );
+
+    anyhow::Ok((activity_service, parser, training_metrics_service))
+}
+
 async fn build_user_service() -> anyhow::Result<
     UserService<
         MagicLinkService<SqliteMagicLinkRepository, SMTPEmailProvider>,
@@ -97,9 +153,13 @@ async fn build_user_service() -> anyhow::Result<
         SessionService<SqliteSessionRepository>,
     >,
 > {
-    let db_dir = load_env("ACTIVITIES_DB_PATH")?;
+    let root_path = PathBuf::from(load_env("ACTIVITIES_DATA_PATH")?);
+    let db_dir = root_path.clone().join("db/");
+    if !db_dir.exists() {
+        tokio::fs::create_dir_all(&db_dir).await?;
+    }
 
-    let magic_db = Path::new(&db_dir).join("magic_link.db");
+    let magic_db = db_dir.clone().join("magic_link.db");
     let magic_link_repository = Arc::new(Mutex::new(
         SqliteMagicLinkRepository::new(&format!("sqlite:{}", magic_db.to_string_lossy())).await?,
     ));
@@ -109,12 +169,12 @@ async fn build_user_service() -> anyhow::Result<
         mail_provider,
     )));
 
-    let user_db = Path::new(&db_dir).join("user.db");
+    let user_db = db_dir.clone().join("user.db");
     let user_repository = Arc::new(Mutex::new(
         SqliteUserRepository::new(&format!("sqlite:{}", user_db.to_string_lossy())).await?,
     ));
 
-    let session_db = Path::new(&db_dir).join("user.db");
+    let session_db = db_dir.clone().join("session.db");
     let session_repository = Arc::new(Mutex::new(
         SqliteSessionRepository::new(&format!("sqlite:{}", session_db.to_string_lossy())).await?,
     ));
