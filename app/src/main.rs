cce72d6608070d25b4015a97ea9187186f9dd975
thomas-god <thomas.godlewski@icloud.com>
2025-11-04 14:02:25 +0100

    fix(app): main entrypoint

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,20 +1,20 @@
-use anyhow::anyhow;
+#[cfg(feature = "multi-user")]
+async fn run() -> anyhow::Result<()> {
+    app::bootstrap::multi_user::bootsrap_multi_user()
+        .await?
+        .run()
+        .await
+}
+
+#[cfg(not(feature = "multi-user"))]
+async fn run() -> anyhow::Result<()> {
+    app::bootstrap::single_user::bootsrap_single_user()
+        .await?
+        .run()
+        .await
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    if cfg!(feature = "single-user") {
-        app::bootstrap::single_user::bootsrap_single_user()
-            .await?
-            .run()
-            .await
-    } else if cfg!(feature = "multi-user") {
-        app::bootstrap::multi_user::bootsrap_multi_user()
-            .await?
-            .run()
-            .await
-    } else {
-        Err(anyhow!(
-            "No feature selected, should be either single-user or mutli-user"
-        ))
-    }
+    run().await
 }
