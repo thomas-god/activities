0d740b66a369298d6920d4e2c14f825cb218ba6f
thomas-god <thomas.godlewski@icloud.com>
2025-11-04 10:09:32 +0100

    feat(client): add helpers to convert speed to pace

---
+++ client/src/lib/speed.test.ts
@@ -0,0 +1,14 @@
+import { expect, it } from 'vitest';
+import { paceToString, speedToPace } from './speed';
+
+it('Should convert a speed from km/h to pace in min/km', () => {
+	expect(speedToPace(12)).toEqual(5);
+	expect(speedToPace(15)).toEqual(4);
+});
+
+it('Should convert a pace to its string representation', () => {
+	expect(paceToString(5)).toEqual('5:00/km');
+	expect(paceToString(4.5)).toEqual('4:30/km');
+	expect(paceToString(10)).toEqual('10:00/km');
+	expect(paceToString(5.56)).toEqual('5:34/km');
+});
---
+++ client/src/lib/speed.ts
@@ -0,0 +1,10 @@
+
+export const speedToPace = (speed:number): number => {
+  return 1 / speed * 60
+}
+
+export const paceToString = (pace: number): string => {
+  const minutes = Math.floor(pace)
+  const seconds = Math.round((pace - minutes) * 60)
+  return `${minutes}:${seconds.toString().padStart(2, "0")}/km`
+}
