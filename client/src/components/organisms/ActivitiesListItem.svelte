8f354bea6b2fd5140a16d449715f5a573068fa23
thomas-god <thomas.godlewski@icloud.com>
2025-11-12 16:23:19 +0100

    feat(client): improve readability of activity's note in activities list view

--- client/src/components/organisms/ActivitiesListItem.svelte
+++ client/src/components/organisms/ActivitiesListItem.svelte
@@ -30,43 +30,45 @@
 
 <a
 	href={`/activity/${activity.id}`}
-	class={`item @container flex flex-1 items-center p-1 pl-2 ${categoryClass(activity.sport_category)}`}
+	class={`item @container py-1 ${categoryClass(activity.sport_category)}`}
 >
-	<div class={`icon ${categoryClass(activity.sport_category)}`}>
-		{getSportCategoryIcon(activity.sport_category)}
-	</div>
-	<div class="flex-1">
-		<div class="flex flex-col">
-			<div class="mb-1 font-semibold">{title}</div>
-			<div class="text-xs font-light">
-				{formatRelativeDuration(dayjs(activity.start_time), dayjs())} · {dayjs(
-					activity.start_time
-				).format('MMM D, YYYY')}
-			</div>
-			{#if showNote && activity.feedback}
-				<div class="mt-2 text-sm text-gray-600 italic">
-					📝 {activity.feedback}
+	<div class={`flex flex-1 items-center pl-2 ${categoryClass(activity.sport_category)}`}>
+		<div class={`icon ${categoryClass(activity.sport_category)}`}>
+			{getSportCategoryIcon(activity.sport_category)}
+		</div>
+		<div class="flex-1">
+			<div class="flex flex-col">
+				<div class="mb-1 font-semibold">{title}</div>
+				<div class="text-xs font-light">
+					{formatRelativeDuration(dayjs(activity.start_time), dayjs())} · {dayjs(
+						activity.start_time
+					).format('MMM D, YYYY')}
 				</div>
+			</div>
+		</div>
+		<div class="flex flex-row items-center justify-center gap-2">
+			{#if activity.workout_type}
+				<span
+					class={`badge hidden badge-sm @md:inline ${getWorkoutTypeClass(activity.workout_type)}`}
+				>
+					{getWorkoutTypeLabel(activity.workout_type)}
+				</span>
 			{/if}
+			{#if activity.rpe}
+				<span class={`badge hidden badge-sm @md:inline ${getRpeClass(activity.rpe)}`}>
+					RPE {activity.rpe}
+				</span>
+			{/if}
+			<span class="font-semibold sm:text-lg">
+				{formatDuration(activity.duration)}
+			</span>
 		</div>
 	</div>
-	<div class="flex flex-row items-center justify-center gap-2">
-		{#if activity.workout_type}
-			<span
-				class={`badge hidden badge-sm @md:inline ${getWorkoutTypeClass(activity.workout_type)}`}
-			>
-				{getWorkoutTypeLabel(activity.workout_type)}
-			</span>
-		{/if}
-		{#if activity.rpe}
-			<span class={`badge hidden badge-sm @md:inline ${getRpeClass(activity.rpe)}`}>
-				RPE {activity.rpe}
-			</span>
-		{/if}
-		<span class="font-semibold sm:text-lg">
-			{formatDuration(activity.duration)}
-		</span>
-	</div>
+	{#if showNote && activity.feedback}
+		<div class="my-1 pl-4 text-sm whitespace-pre-wrap text-gray-600 italic">
+			📝 {activity.feedback}
+		</div>
+	{/if}
 </a>
 
 <style>
