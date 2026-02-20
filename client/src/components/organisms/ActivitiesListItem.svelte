f6a07e162652deab9cb79e58a5dba343e4f51fe9
thomas-god <thomas.godlewski@icloud.com>
2026-02-20 17:21:08 +0100

    fix(client): activity feedback and note's content could shrink icon

--- client/src/components/organisms/ActivitiesListItem.svelte
+++ client/src/components/organisms/ActivitiesListItem.svelte
@@ -78,7 +78,7 @@
 		<div
 			class="mx-3 my-1 box-border flex flex-row items-start gap-1 bg-orange-200/10 py-2 pl-2 text-sm whitespace-pre-wrap text-gray-600 italic"
 		>
-			<div><img src="/icons/note.svg" class="h-5 w-5" alt="Memo icon" /></div>
+			<div class="shrink-0"><img src="/icons/note.svg" class="h-5 w-5" alt="Memo icon" /></div>
 			<div>
 				{activity.feedback}
 			</div>
--- client/src/components/organisms/TrainingNoteListItemCompact.svelte
+++ client/src/components/organisms/TrainingNoteListItemCompact.svelte
@@ -92,7 +92,7 @@
 	<div
 		class="mx-1 my-1 box-border flex flex-row gap-1 bg-orange-200/10 py-2 pl-2 text-sm whitespace-pre-wrap text-gray-600 italic"
 	>
-		<div><img src="/icons/note.svg" class="h-5 w-5" alt="Memo icon" /></div>
+		<div class="shrink-0"><img src="/icons/note.svg" class="h-5 w-5" alt="Memo icon" /></div>
 		<div>
 			{note.content}
 		</div>
