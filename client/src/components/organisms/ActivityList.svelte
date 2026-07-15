a91e65abd33ac56e0b7e22c787631798781c0e28
thomas-god <thomas.godlewski@icloud.com>
2026-07-15 18:52:21 +0200

    feat(client): use lazy-keep-alive rendering strategy for activity list variants

--- client/src/components/organisms/ActivityList.svelte
+++ client/src/components/organisms/ActivityList.svelte
@@ -4,6 +4,8 @@
 	import TrainingNoteListItemCompact from '$components/organisms/TrainingNoteListItemCompact.svelte';
 	import { toTitleCase } from '$lib/utils';
 	import ActivityListItemWithDetails from './ActivityListItemWithDetails.svelte';
+	import LazyKeepAliveObserved from '$components/atoms/utils/LazyKeepAliveObserved.svelte';
+	import ResponsiveSwitch from '$components/atoms/utils/ResponsiveSwitch.svelte';
 
 	let {
 		selectedActivityId,
@@ -48,87 +50,93 @@
 			}
 		})
 	);
-	let containerWidth = $state(0);
 </script>
 
-<div class="@container" bind:clientWidth={containerWidth}>
+<div class="@container">
 	{#if timeline.size > 0}
-		{#if containerWidth > 650}
-			<!-- Larger screen widths -->
-			<div
-				class="grid-container"
-				style:grid-template-columns={`minmax(auto, 60px) minmax(175px, 1fr) ${summaryTemplate}`}
-			>
-				<!-- Items -->
-				{#each timeline as [group, items], idx}
-					<div
-						class="sticky-left bg-base-100 py-2 text-xs font-semibold tracking-wide text-base-content/60 uppercase"
-						style:grid-column={'1 / span 2'}
-					>
-						{group}
-						{#if showGroupNumberOfActivities}
-							&nbsp - {items.filter((item) => item.type === 'activity').length} activities
-						{/if}
-					</div>
-					<!-- Metrics headers aligned to first group of the timeline -->
-					{#if idx === 0}
-						{#each summaryHeaders as header, header_index}
-							<div style:grid-column-start={header_index + 1 + 2} class="text-xs text-center py-2">
-								{header}
-							</div>
-						{/each}
-					{/if}
-					{#each items as item (item.date)}
-						{#if item.type === 'activity'}
-							<ActivityListItemWithDetails
-								activity={item.data}
-								onClick={() => {
-									if (selectActivityCallback !== undefined) {
-										selectActivityCallback(item.data.id);
-									}
-								}}
-								isSelected={selectedActivityId === item.data.id}
-								listFormat={activityListFormat}
-							/>
-						{:else}
-							<TrainingNoteListItemCompact note={item.data} />
-						{/if}
-					{/each}
-				{/each}
-			</div>
-		{:else}
-			<!-- Smaller screen widths -->
-			<div class="small-container">
-				{#each timeline as [group, items], idx}
-					<div
-						class="bg-base-100 py-2 text-xs font-semibold tracking-wide text-base-content/60 uppercase"
-					>
-						{group}
-						{#if showGroupNumberOfActivities}
-							&nbsp - {items.filter((item) => item.type === 'activity').length} activities
-						{/if}
-					</div>
+		<ResponsiveSwitch breakpoint={650}>
+			{#snippet full()}
+				<LazyKeepAliveObserved name={'grid'}>
+					{#snippet children()}
+						<!-- Larger screen widths -->
+						<div
+							class="grid-container"
+							style:grid-template-columns={`minmax(auto, 60px) minmax(175px, 1fr) ${summaryTemplate}`}
+						>
+							<!-- Items -->
+							{#each timeline as [group, items], idx}
+								<div
+									class="sticky-left bg-base-100 py-2 text-xs font-semibold tracking-wide text-base-content/60 uppercase"
+									style:grid-column={'1 / span 2'}
+								>
+									{group}
+									{#if showGroupNumberOfActivities}
+										&nbsp - {items.filter((item) => item.type === 'activity').length} activities
+									{/if}
+								</div>
+								<!-- Metrics headers aligned to first group of the timeline -->
+								{#if idx === 0}
+									{#each summaryHeaders as header, header_index}
+										<div
+											style:grid-column-start={header_index + 1 + 2}
+											class="text-xs text-center py-2"
+										>
+											{header}
+										</div>
+									{/each}
+								{/if}
+								{#each items as item (item.date)}
+									{#if item.type === 'activity'}
+										<ActivityListItemWithDetails
+											activity={item.data}
+											onClick={() => selectActivityCallback(item.data.id)}
+											isSelected={selectedActivityId === item.data.id}
+											listFormat={activityListFormat}
+										/>
+									{:else}
+										<TrainingNoteListItemCompact note={item.data} />
+									{/if}
+								{/each}
+							{/each}
+						</div>
+					{/snippet}
+				</LazyKeepAliveObserved>
+			{/snippet}
 
-					{#each items as item (item.date)}
-						{#if item.type === 'activity'}
-							<div class="flex flex-row">
-								<ActivityListItem
-									activity={item.data}
-									onClick={() => {
-										if (selectActivityCallback !== undefined) {
-											selectActivityCallback(item.data.id);
-										}
-									}}
-									isSelected={selectedActivityId === item.data.id}
-								/>
-							</div>
-						{:else}
-							<TrainingNoteListItemCompact note={item.data} />
-						{/if}
-					{/each}
-				{/each}
-			</div>
-		{/if}
+			{#snippet compact()}
+				<LazyKeepAliveObserved name={'list'}>
+					{#snippet children()}
+						<!-- Smaller screen widths -->
+						<div class="small-container">
+							{#each timeline as [group, items], idx}
+								<div
+									class="bg-base-100 py-2 text-xs font-semibold tracking-wide text-base-content/60 uppercase"
+								>
+									{group}
+									{#if showGroupNumberOfActivities}
+										&nbsp - {items.filter((item) => item.type === 'activity').length} activities
+									{/if}
+								</div>
+
+								{#each items as item (item.date)}
+									{#if item.type === 'activity'}
+										<div class="flex flex-row">
+											<ActivityListItem
+												activity={item.data}
+												onClick={() => selectActivityCallback(item.data.id)}
+												isSelected={selectedActivityId === item.data.id}
+											/>
+										</div>
+									{:else}
+										<TrainingNoteListItemCompact note={item.data} />
+									{/if}
+								{/each}
+							{/each}
+						</div>
+					{/snippet}
+				</LazyKeepAliveObserved>
+			{/snippet}
+		</ResponsiveSwitch>
 	{:else}
 		<div class="py-8 text-center text-sm italic opacity-70">No activities or notes found</div>
 	{/if}
@@ -138,19 +146,12 @@
 	.small-container {
 		display: flex;
 		flex-direction: column;
-		@container (width >= 650px) {
-			display: none;
-		}
 	}
 
 	.grid-container {
 		display: grid;
 		width: 100%;
 		overflow-x: scroll;
-
-		@container (width < 650px) {
-			display: none;
-		}
 	}
 
 	.sticky-left {
