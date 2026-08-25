78e551d7ab27534fa254b77b8429f8452a622b6e
thomas-god <thomas.godlewski@icloud.com>
2026-08-25 18:01:56 +0200

    fix(client): always collapse navbar's ctas

--- client/src/ui/navigation/Navbar.svelte
+++ client/src/ui/navigation/Navbar.svelte
@@ -57,33 +57,30 @@
 	</div>
 
 	<div class="flex shrink-0 items-center gap-3">
-		{#if ctas.length > 0}
-			<div class="hidden flex-row justify-end gap-2 min-[850px]:flex">
-				{#each ctas as cta, idx (cta.label)}
-					<button
-						class={`btn rounded-lg btn-sm sm:btn-md ${idx === 0 ? 'btn-primary' : 'btn-outline btn-primary'}`}
-						onclick={cta.onClick}>+ {cta.label}</button
-					>
-				{/each}
-			</div>
-			<div class="dropdown dropdown-end min-[850px]:hidden">
-				<button tabindex="0" class="btn btn-outline btn-primary btn-sm" aria-label="Quick actions">
-					<img src="/icons/menu.svg" class="h-5 w-5" alt="Menu icon" />
-				</button>
-				<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-				<ul tabindex="0" class="menu dropdown-content z-1 w-48 rounded-box bg-base-100 p-2 shadow">
-					{#each mobileMenuItems as item (item.label)}
-						<li><button onclick={item.onClick}>{item.label}</button></li>
-					{/each}
-				</ul>
-			</div>
-		{/if}
+		<div class="hidden flex-row justify-end gap-2 min-[850px]:flex">
+			{#each ctas as cta, idx (cta.label)}
+				<button
+					class={`btn rounded-lg btn-sm sm:btn-md ${idx === 0 ? 'btn-primary' : 'btn-outline btn-primary'}`}
+					onclick={cta.onClick}>+ {cta.label}</button
+				>
+			{/each}
+		</div>
 		{#if showLogout}
-			<button
-				class={`btn btn-ghost btn-sm ${ctas.length > 0 ? 'hidden min-[850px]:flex' : ''}`}
-				onclick={handleLogout}>Log out</button
+			<button class="btn hidden btn-ghost btn-sm min-[850px]:flex" onclick={handleLogout}
+				>Log out</button
 			>
 		{/if}
+		<div class="dropdown dropdown-end min-[850px]:hidden">
+			<button tabindex="0" class="btn btn-outline btn-primary btn-sm" aria-label="Quick actions">
+				<img src="/icons/menu.svg" class="h-5 w-5" alt="Menu icon" />
+			</button>
+			<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+			<ul tabindex="0" class="menu dropdown-content z-1 w-48 rounded-box bg-base-100 p-2 shadow">
+				{#each mobileMenuItems as item (item.label)}
+					<li><button onclick={item.onClick}>{item.label}</button></li>
+				{/each}
+			</ul>
+		</div>
 	</div>
 </div>
 
